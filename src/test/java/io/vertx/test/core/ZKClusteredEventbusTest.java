0bcbb54d24e981a865e7a398608085938ad6f255
stream <stream1984@gmail.com>
2017-05-05 11:47:12 +0800

    delay verticle node start.
    Signed-off-by: stream <stream1984@gmail.com>

--- src/test/java/io/vertx/test/core/ZKClusteredEventbusTest.java
+++ src/test/java/io/vertx/test/core/ZKClusteredEventbusTest.java
@@ -1,8 +1,11 @@
 package io.vertx.test.core;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.zookeeper.MockZKCluster;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,6 +21,36 @@ public class ZKClusteredEventbusTest extends ClusteredEventBusTest {
       zkClustered = new MockZKCluster();
     } catch (Exception e) {
       e.printStackTrace();
+    }
+  }
+
+  protected void startNodes(int numNodes, VertxOptions options) {
+    CountDownLatch latch = new CountDownLatch(numNodes);
+    vertices = new Vertx[numNodes];
+    for (int i = 0; i < numNodes; i++) {
+      int index = i;
+      clusteredVertx(options.setClusterHost("localhost").setClusterPort(0).setClustered(true)
+        .setClusterManager(getClusterManager()), ar -> {
+        try {
+          if (ar.failed()) {
+            ar.cause().printStackTrace();
+          }
+          assertTrue("Failed to start node", ar.succeeded());
+          vertices[index] = ar.result();
+        } finally {
+          latch.countDown();
+        }
+      });
+    }
+    try {
+      Thread.sleep(1500L);
+    } catch (InterruptedException e) {
+      //
+    }
+    try {
+      assertTrue(latch.await(2, TimeUnit.MINUTES));
+    } catch (InterruptedException e) {
+      fail(e.getMessage());
     }
   }
 
